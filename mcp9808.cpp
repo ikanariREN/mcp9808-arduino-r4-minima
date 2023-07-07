a85ffc1c5cde12b405ae6b53a9ba3385f65a510f
Irini Kanari <irini.kanari.xm@renesas.com>
2023-07-07 15:27:41 +0300

    Library for reading MCP9808

--- mcp9808.cpp
+++ mcp9808.cpp
@@ -4,6 +4,7 @@
 //Constructor
 MCP9808::MCP9808(){}
 
+/*Get sensor's MAN ID and check if is detected*/
 bool MCP9808::isDetected(void)
 {
 	int id = 0;
@@ -20,6 +21,7 @@ bool MCP9808::isDetected(void)
 
 		id |= Wire.read(); // receive low byte as lower 8 bits
 	}
+	/*--If id is 1024 (0x400 in hex) sensor is detected*/
 	if(id == 1024){
 		this->id = id;
 		return true;
@@ -27,11 +29,13 @@ bool MCP9808::isDetected(void)
 	return false;	
 } 
 
+/*Join I2C Bus*/
 void MCP9808::begin(void)
 {
-  Wire.begin();
+	Wire.begin();
 }
 
+/*Read raw temperature values from MCP9808*/
 void MCP9808::getRawTemp(void)
 { 
 	uint16_t temperature;
@@ -48,6 +52,7 @@ void MCP9808::getRawTemp(void)
 	}
 }
 
+/*Calculate float temperature*/
 void MCP9808::calculateFloatTemp(void)
 {
 	float floatTemp = 0.0;
@@ -60,6 +65,7 @@ void MCP9808::calculateFloatTemp(void)
 	this->temperature = floatTemp;
 }
 
+/* Getters*/
 float MCP9808::getFloatTemp()
 {
   return this->temperature;
@@ -70,19 +76,21 @@ int MCP9808::getMCP9808id(void)
   return this->id;
 }
 
+/*Set sensor to sleep mode*/
 void MCP9808::setSleepMode(uint16_t cmd)
 {
-  cmd = 0x100;
-	uint16_t buffToSend[2] ={MCP9808_REG_CFG,cmd}; //cmd=0x100;
+	cmd = 0x100; //cmd to sleep
+	uint16_t buffToSend[2] ={MCP9808_REG_CFG,cmd}; 
 	Wire.beginTransmission(MCP9808_SLAVE_ADD);
 	Wire.write((uint8_t*)buffToSend,4);
 	Wire.endTransmission();	
 }
 
+/*Wake up sensor from sleep mode*/
 void MCP9808::setWakeUpMode(uint16_t cmd)
 {
-  cmd = 0x0000;
-	uint16_t buffToSend[2] ={MCP9808_REG_CFG,cmd}; //cmd=0x0000;
+    cmd = 0x0000; //cmd to wake up
+	uint16_t buffToSend[2] ={MCP9808_REG_CFG,cmd}; 
 	Wire.beginTransmission(MCP9808_SLAVE_ADD);
 	Wire.write((uint8_t*)buffToSend,4);
 	Wire.endTransmission();
